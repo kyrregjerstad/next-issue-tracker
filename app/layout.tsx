d0963842f260a52a5da0f24e4594118e9be02858
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-10-22 11:28:49 +0200

    add md editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
-import "@radix-ui/themes/styles.css";
-
 import "./globals.css";
+import "./theme-config.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import { Theme } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className={inter.variable}>
+      <body>
         <Theme>
           <NavBar />
           <main className="p-5">{children}</main>
