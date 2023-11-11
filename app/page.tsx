f4a510abd87a73f53e13000e677455bd554389ba
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-11-11 11:56:42 +0100

    Add responsive layout to Home page with Flex and Grid components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
 export default async function Home({ searchParams }: { searchParams: { page: string } }) {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -10,8 +11,13 @@ export default async function Home({ searchParams }: { searchParams: { page: str
 
   return (
     <>
-      {/* <IssueSummary open={open} inProgress={inProgress} closed={closed} /> */}
-      <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      <Grid columns={{ initial: "1", md: "2" }} gap="5">
+        <Flex direction="column" gap="5">
+          <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+          <IssueChart open={open} inProgress={inProgress} closed={closed} />
+        </Flex>
+        <LatestIssues />
+      </Grid>
     </>
   );
 }
